6a2383645f83b15b1a7e93eb375b1a75476a264e
sJhonny-e <jhonny.ed@cryptolab.net>
2021-07-11 14:49:58 +0100

    add compatibility method
    so that using code can still use this new class without changing

--- app/point.rb
+++ app/point.rb
@@ -1,7 +1,12 @@
 class Point
     attr_reader :x, :y
-    
+
     def initialize(x,y)
         @x = x; @y = y    
+    end
+
+    # TODO: delete when refactoring is done
+    def to_arr
+        [x,y]
     end
 end
